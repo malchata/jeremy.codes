55db2636a99ab105008c75a5b216d8221aab4da9
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2019-07-15 13:44:20 -0500

    Turns out aria-hidden doesn't work unless you give it a value of true. D'oh.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,9 +6,9 @@ export default props => (
       <a href="/" className="header__site-name" rel="noopener">&gt; Jeremy Wagner</a>
       <nav className="header__nav">
         <a className={`header__nav-link ${props.slug === "writing" || props.slug === "blog" ? "header__nav-link--on" : ""}`} href="/writing/" rel="noopener">Writing</a>
-        <span className="header__nav-separator" aria-hidden>::</span>
+        <span className="header__nav-separator" aria-hidden="true">::</span>
         <a className="header__nav-link" href="https://speaking.jeremy.codes/" rel="noopener">Speaking</a>
-        <span className="header__nav-separator" aria-hidden>::</span>
+        <span className="header__nav-separator" aria-hidden="true">::</span>
         <a className={`header__nav-link ${props.slug === "contact" ? "header__nav-link--on" : ""}`} href="/contact/" rel="noopener">Get in touch</a>
       </nav>
     </div>
