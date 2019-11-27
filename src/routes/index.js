62c6ab287f55dd8c3c495670e0f86a007c0b093b
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2019-11-26 20:20:31 -0600

    Minor tweak.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import Home from "../components/Home";
 
 export const Metadata = {
-  title: "Welcome",
-  description: "The home page of web performance consultant, developer, speaker, and author.",
+  title: "",
+  description: "",
   date: "14 January, 2019",
   lastBuildDate: "14 July, 2019",
   slug: "/",
