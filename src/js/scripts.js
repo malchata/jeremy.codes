6a399762e263177f637efab0924e24385ecb1b8f
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2019-12-13 12:47:51 -0600

    Removed yall, doing native lazy loading.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,12 @@
 import quicklink from "quicklink";
 import dnstradamus from "dnstradamus";
-import yall from "yall-js";
 
 document.addEventListener("DOMContentLoaded", () => {
   quicklink({
-    ignores: uri => /\.(xml|jpe?g|gif|png|svg|m?jsx?|css)$/i.test(uri)
+    ignores: uri => /\.(xml|jpe?g|webp|gif|png|svg|m?jsx?|css)$/i.test(uri)
   });
 
   if ([].slice.call(document.querySelectorAll("a[href^=\"http://\"],a[href^=\"https://\"]")).length) {
     dnstradamus();
-  }
-
-  if ([].slice.call(document.querySelectorAll(".lazy")).length) {
-    yall();
   }
 });
